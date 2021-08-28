c0178f433c378e76f132715ca2edd353cfcf1705
bradleykenny <bkenny283@gmail.com>
2021-08-28 15:26:43 +1000

    Got markdown rendering on the page properly.

--- pages/user/[user]/index.tsx
+++ pages/user/[user]/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { useRouter } from "next/router";
+import parser from "html-react-parser";
+import marked from "marked";
 
 const User = () => {
 	const router = useRouter();
 	const { user } = router.query;
 
-	return <div>{user}</div>;
+	const hardcodedMarkdownWithQueryName = `# Hello, ${user}\nThis is your customised page.`;
+
+	return <div>{parser(marked(hardcodedMarkdownWithQueryName))}</div>;
 };
 
 export default User;
