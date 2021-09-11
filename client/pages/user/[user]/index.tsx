00d175e3159991387377024bacdf30f94f0a41f6
bradleykenny <bkenny283@gmail.com>
2021-09-11 16:54:19 +1000

    Don't even need to create a custom server :( Next has one built in...

--- client/pages/user/[user]/index.tsx
+++ client/pages/user/[user]/index.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
-import parser from "html-react-parser";
-import marked from "marked";
+import axios, { AxiosResponse } from "axios";
 
 import MarkdownCard from "../../../components/MarkdownCard";
 
@@ -9,10 +8,23 @@ const User = () => {
 	const router = useRouter();
 	const { user } = router.query;
 
+	const [name, setName] = useState("");
+
 	const hardcodedMarkdownWithQueryName = `# Hello, ${user}\nThis is your customised page.`;
+
+	const result: Promise<AxiosResponse<{ name: string }>> = axios.get(
+		"http://localhost:3000/api/hello"
+	);
+	let something: string = "what";
+	result.then((res) => {
+		setName(res.data.name);
+		console.log(something);
+	});
 
 	return (
 		<div>
+			{"why isnt this showing, " + name}
+			<h1>Hello</h1>
 			<MarkdownCard markdown={hardcodedMarkdownWithQueryName} />
 		</div>
 	);
