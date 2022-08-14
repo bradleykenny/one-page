b923b5e342b0188379e63bb0f38996a5788ab3f0
bradleykenny <bkenny283@gmail.com>
2022-08-14 22:11:24 +1000

    Tailwind added..

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import MarkdownCard from "../components/MarkdownCard";
+import styles from "../styles/Home.module.css";
 
 const someMarkdown = `
 # Heading
