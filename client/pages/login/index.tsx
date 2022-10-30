231c02778bea5cca8e84e74f249163ec8eaf4027
bradleykenny <bkenny283@gmail.com>
2022-10-30 17:38:00 +1100

    Updating login page look

--- client/pages/login/index.tsx
+++ client/pages/login/index.tsx
@@ -22,19 +22,19 @@ const Login = () => {
                     rel="stylesheet"
                 />
             </Head>
-            <div className="grid grid-cols-2 h-screen">
-                <div className="bg-gradient-to-br from-indigo-500 to-orange-500 drop-shadow-xl z-10 flex justify-center">
-                    <h1 className="text-center text-4xl text-white self-center justify-center">
+            <div className="grid grid-cols-2 h-screen bg-gray-200">
+                <div className="bg-gradient-to-br from-indigo-500 to-orange-500 drop-shadow-md z-10 flex justify-center m-20 mr-10 rounded-lg flex-col">
+                    <h1 className="text-center text-4xl text-white self-center justify-center opacity-40 my-5">
                         Text editing.
                     </h1>
-                    <h1 className="text-center text-4xl text-white self-center justify-center">
+                    <h1 className="text-center text-5xl text-white self-center justify-center opacity-70 my-5">
                         In a new way.
                     </h1>
-                    <h1 className="text-center text-4xl text-white self-center justify-center">
+                    <h1 className="text-center text-6xl text-white self-center justify-center my-5">
                         A Better way.
                     </h1>
                 </div>
-                <div className="bg-gray-200 z-0 flex">
+                <div className="z-0 bg-white drop-shadow-md flex justify-center m-20 ml-10 rounded-lg flex-col">
                     <div className="w-1/2 self-center bg-white rounded-md shadow mx-auto flex flex-col p-4">
                         <h1 className="text-center font-cursive text-4xl mt-3 mb-4 text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-orange-500">
                             Welcome back
