d40255a79b9a7b26459d9ebeb48d86d654ea6072
bradleykenny <bkenny283@gmail.com>
2022-11-02 22:55:45 +1100

    Updating loging page. Trying temp background image effect.

--- client/pages/login/index.tsx
+++ client/pages/login/index.tsx
@@ -1,9 +1,12 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Login = () => {
     const router = useRouter();
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     useEffect(() => {
         const isLoggedIn = false;
@@ -11,6 +14,18 @@ const Login = () => {
             router.push("/home");
         }
     }, []);
+
+    const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = () => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+    };
 
     return (
         <div>
@@ -25,34 +40,40 @@ const Login = () => {
             <div className="grid grid-cols-2 h-screen bg-gray-200">
                 <div className="bg-gradient-to-br from-indigo-500 to-orange-500 drop-shadow-md z-10 flex justify-center m-20 mr-10 rounded-lg flex-col">
                     <h1 className="text-center text-4xl text-white self-center justify-center opacity-40 my-5">
-                        Text editing.
+                        Editing.
                     </h1>
                     <h1 className="text-center text-5xl text-white self-center justify-center opacity-70 my-5">
-                        In a new way.
+                        Different.
                     </h1>
                     <h1 className="text-center text-6xl text-white self-center justify-center my-5">
-                        A Better way.
+                        Start today.
                     </h1>
                 </div>
                 <div className="z-0 bg-white drop-shadow-md flex justify-center m-20 ml-10 rounded-lg flex-col">
-                    <div className="w-1/2 self-center bg-white rounded-md shadow mx-auto flex flex-col p-4">
-                        <h1 className="text-center font-cursive text-4xl mt-3 mb-4 text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-orange-500">
-                            Welcome back
+                    <div className="h-full w-full absolute bg-login-bg grayscale opacity-5 bg-cover"></div>
+
+                    <form
+                        className="w-1/2 self-center mx-auto flex flex-col z-0"
+                        onSubmit={handleSubmit}>
+                        <h1 className="text-center font-cursive text-6xl mb-5 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-orange-500">
+                            Welcome back!
                         </h1>
                         <input
                             type="text"
                             placeholder="Username"
-                            className="px-2 py-2 m-2 border rounded-md focus:shadow focus:outline-none"
+                            onChange={handleUsernameChange}
+                            className="p-2 m-2 border rounded-md focus:outline-2 focus:outline-orange-500"
                         />
                         <input
                             type="password"
                             placeholder="Password"
-                            className="px-2 py-2 m-2 border rounded-md focus:shadow focus:outline-none"
+                            onChange={handlePasswordChange}
+                            className="p-2 m-2 border rounded-md focus:outline-2 focus:outline-orange-500"
                         />
-                        <button className="px-2 py-2 m-2 bg-orange-500 rounded-md text-white hover:shadow hover:bg-orange-600">
+                        <button className="p-2 m-2 bg-orange-500 rounded-md text-white hover:shadow hover:bg-orange-600">
                             Login
                         </button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
             fontFamily: {
                 cursive: ['"Gloria Hallelujah"'],
             },
+            backgroundImage: {
+                "login-bg":
+                    "url('https://static.vecteezy.com/system/resources/previews/002/868/201/large_2x/hand-drawn-webinar-set-doodle-background-free-vector.jpg')",
+            },
         },
     },
     plugins: [require("@tailwindcss/typography")],
