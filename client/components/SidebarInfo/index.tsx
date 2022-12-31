d8492e76e1ccd3ef26e654d8366fda8e73bee0b2
bradleykenny <bkenny283@gmail.com>
2022-12-31 18:10:36 +1100

    Expanding sidebar

--- client/components/SidebarInfo/index.tsx
+++ client/components/SidebarInfo/index.tsx
@@ -5,16 +5,31 @@ interface IProps {
 }
 
 const SidebarInfo = (props: IProps) => {
+    const { page } = props;
+
     return (
         <aside
-            className="w-72 fixed top-24 bottom-4 right-4 overflow-y-scroll px-1 pb-10 z-0 bg-white shadow rounded-lg hover:shadow-md"
+            className="w-72 fixed top-24 right-4 overflow-y-scroll px-1 pb-4 z-0 bg-white shadow rounded-lg hover:shadow-md"
             aria-label="Sidebar">
             <h2 className="text-lg font-bold mb-2 ml-4 py-2">Information</h2>
             <ul className="space-y-2 px-4">
-                <li className="mb-4">Posted by ...</li>
+                <li>
+                    <h3>User</h3>
+                    Posted by <u>{page?.userId}</u>
+                </li>
                 <li>
                     <h3>Project</h3>
-                    <p>Example</p>
+                    <p className="rounded-lg bg-green-700 mt-1 px-2 py-1 inline-block text-sm text-white cursor-pointer">
+                        {"Example".toUpperCase()}
+                    </p>
+                </li>
+                <li>
+                    <h3>Last Updated</h3>
+                    <p>{new Date(page?.updatedAt).toDateString()}</p>
+                </li>
+                <li>
+                    <h3>Created On</h3>
+                    <p>{new Date(page?.createdAt).toDateString()}</p>
                 </li>
             </ul>
         </aside>
